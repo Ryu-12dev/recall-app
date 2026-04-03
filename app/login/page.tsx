a6eb4f40f9bb2b165b02f15249a1bb0bb5918392
shiryu-tanaka <shiryu.1211@icloud.com>
2026-04-03 12:38:54 +0900

    feat: created login ui

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,12 +1,25 @@
+import Image from "next/image";
 import { signInWithGoogle } from "./action";
 
 export default function LoginPage() {
   return (
-    <div className="border rounded-md">
-      <h1 className="text-teal-600 text-5xl font-bold">Recall</h1>
-      <form>
-        <button formAction={signInWithGoogle}>ログイン</button>
-      </form>
+    <div className="flex items-center justify-center min-h-screen bg-stone-50">
+      <div className="border border-gray-200 rounded-xl p-15 shadow-xl text-center bg-white">
+        <header className="flex items-center justify-center pb-4">
+          <Image src="/favicon.ico" alt="favicon" width={35} height={35} className="mr-2"/>
+          <h1 className="text-black text-4xl font-bold">Recall</h1>
+        </header>
+        <p className="text-stone-400 mb-5">効率的な学習を手に入れましょう。</p>
+        <form>
+          <button className="hover:cursor-pointer hover:bg-zinc-100 border border-gray-200 px-10 py-2 rounded-4xl"
+            formAction={signInWithGoogle}>
+            <div className="flex items-center gap-2">
+              <Image src="/google.svg" alt="google" width={25} height={25} />
+              <span>Googleでログイン</span>
+            </div>
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
