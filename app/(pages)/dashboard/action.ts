dd2ab3515b1fc1d8ec3fec3b1b20a96532006253
shiryu-tanaka <shiryu.1211@icloud.com>
2026-04-09 00:55:29 +0900

    feature: add deck

---
+++ app/(pages)/dashboard/action.ts
@@ -0,0 +1,20 @@
+"use server"
+
+import { prisma } from "@/lib/prisma/prisma";
+import { createClient } from "@/lib/supabase/server";
+
+export default async function addDeck(name: string) {
+  const supabase = await createClient();
+  const { data: { user } } = await supabase.auth.getUser();
+
+  if (!user) {
+    throw new Error("ログインが必要です");
+  }
+
+  return await prisma.decks.create({
+    data: {
+      userId: user.id,
+      name,
+    }
+  });
+}
