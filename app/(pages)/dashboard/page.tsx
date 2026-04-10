c17d3728fa3df838403bb1d933990f3440a304a7
shiryu-tanaka <shiryu.1211@icloud.com>
2026-04-10 15:00:00 +0900

    prisma.tsをシングルトン化

--- app/(pages)/dashboard/page.tsx
+++ app/(pages)/dashboard/page.tsx
@@ -1,13 +1,26 @@
 import DashboardClient from "./DashboardClient";
 import AddDeckModal from "./AddDeckModal";
+import { prisma } from "@/lib/prisma/prisma";
+import { createClient } from "@/lib/supabase/server";
 
-export default function Dashboard() {
+export default async function Dashboard() {
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  const decks = await prisma.decks.findMany({
+    where: {
+      userId: user?.id,
+    }
+  });
+
   return (
     <div className="pl-20 pt-10 pr-20">
       <header className="mb-10">
         <AddDeckModal />
       </header>
       <main className="">
+        <DashboardClient decks={decks} />
         <div
           className="hover:border-blue-600 hover:cursor-pointer bg-white border border-slate-100
           border-3 rounded-2xl p-3"
--- lib/prisma/prisma.ts
+++ lib/prisma/prisma.ts
@@ -1,7 +1,61 @@
-import { PrismaClient } from "@/app/generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
-});
-export const prisma = new PrismaClient({ adapter });
+// import { PrismaClient } from "@/app/generated/prisma/client";
+// import { PrismaPg } from "@prisma/adapter-pg";
+// const adapter = new PrismaPg({
+//   connectionString: process.env.DATABASE_URL!,
+// });
+// export const prisma = new PrismaClient({ adapter });
 
+// import { PrismaClient } from "@/app/generated/prisma/client";
+// import { PrismaPg } from "@prisma/adapter-pg";
+//
+// const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+//
+// const adapter = new PrismaPg({
+//   connectionString: process.env.DATABASE_URL!,
+// });
+//
+// export const prisma =
+//   globalForPrisma.prisma ?? new PrismaClient({ adapter });
+//
+// if (process.env.NODE_ENV !== "production") {
+//   globalForPrisma.prisma = prisma;
+
+// import { PrismaClient } from "@/app/generated/prisma/client";
+// import { PrismaPg } from "@prisma/adapter-pg";
+// import { Pool } from "pg";
+//
+// const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+//
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL!,
+// });
+//
+// const adapter = new PrismaPg(pool);
+//
+// export const prisma =
+//   globalForPrisma.prisma ?? new PrismaClient({ adapter });
+//
+// if (process.env.NODE_ENV !== "production") {
+//   globalForPrisma.prisma = prisma;
+
+
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@/app/generated/prisma/client";
+
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter,
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+} 
