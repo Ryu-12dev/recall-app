c23084a0d0fcbcd8953cbd557e920f3f235fa4c6
shiryu-tanaka <shiryu.1211@icloud.com>
2026-04-11 14:53:14 +0900

    デッキの取得処理をDashboardContentに移行

--- app/(pages)/dashboard/page.tsx
+++ app/(pages)/dashboard/page.tsx
@@ -1,18 +1,8 @@
-import DashboardClient from "./DashboardClient";
+import DashboardContent from "./DashboardContent";
 import AddDeckModal from "./AddDeckModal";
-import { prisma } from "@/lib/prisma/prisma";
-import { createClient } from "@/lib/supabase/server";
+import { Suspense } from "react";
 
 export default async function Dashboard() {
-  const supabase = await createClient();
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-  const decks = await prisma.decks.findMany({
-    where: {
-      userId: user?.id,
-    }
-  });
 
   return (
     <div className="pl-20 pt-10 pr-20">
@@ -20,13 +10,11 @@ export default async function Dashboard() {
         <AddDeckModal />
       </header>
       <main className="">
-        <DashboardClient decks={decks} />
-        <div
-          className="hover:border-blue-600 hover:cursor-pointer bg-white border border-slate-100
-          border-3 rounded-2xl p-3"
-        >
-          <h1 className="text-3xl">Title</h1>
-        </div>
+        <nav>
+          <Suspense>
+            <DashboardContent />
+          </Suspense>
+        </nav>
       </main>
     </div>
   );
