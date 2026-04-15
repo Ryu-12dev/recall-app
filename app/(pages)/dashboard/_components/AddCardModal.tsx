cbda3198a27d19833a7785946eb00929b799a70e
shiryu-tanaka <shiryu.1211@icloud.com>
2026-04-15 23:52:48 +0900

    テキストサイズを修正

--- app/(pages)/dashboard/_components/AddCardModal.tsx
+++ app/(pages)/dashboard/_components/AddCardModal.tsx
@@ -1,24 +1,23 @@
-
 export default function AddCardModal({ id }: { id: string }) {
   return (
     <>
-      <h2 className="text-lg font-bold mb-4">カード追加</h2>
-      <div>
-        <form
-          className="flex flex-col gap-4"
-        >
-          <div>
-            <p className="text-base mb-2">表</p>
-            <textarea 
-              ref={el => el?.focus()}
-              className="border rounded-xl text-3xl" />
-          </div>
-          <div>
-            <p className="text-base mb-2">裏</p>
-            <textarea className="border rounded-xl text-3xl" />
-          </div>
-        </form>
-      </div>
+      <header>
+        <h2 className="text-lg font-bold mb-4">カード追加</h2>
+      </header>
+      <form
+        className="flex flex-col gap-4"
+      >
+        <div>
+          <p className="text-base mb-2">表</p>
+          <textarea 
+            ref={el => el?.focus()}
+            className="border rounded-lg text-2xl" />
+        </div>
+        <div>
+          <p className="text-base mb-2">裏</p>
+          <textarea className="border rounded-lg text-2xl" />
+        </div>
+      </form>
     </>
   );
 }
